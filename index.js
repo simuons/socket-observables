5cb7f56677e35088f1d7b18e4f4d04de445f6ddd
Simonas Pinevičius <simonasp@wix.com>
2015-12-29 14:56:55 +0200

    Use es6 syntax in exports

--- index.js
+++ index.js
@@ -2,22 +2,20 @@
 
 let Rx = require("rx");
 
-exports.udp = function (socket) {
-    return Rx.Observable.create(observer => {
+exports.udp = socket => Rx.Observable.create(observer => {
 
-        let onNext = message => observer.onNext(message);
-        let onError = error => observer.onError(error);
-        let onCompleted = () => observer.onCompleted();
+    let onNext = message => observer.onNext(message);
+    let onError = error => observer.onError(error);
+    let onCompleted = () => observer.onCompleted();
 
-        socket.on("message", onNext);
-        socket.on("error", onError);
-        socket.on("close", onCompleted);
+    socket.on("message", onNext);
+    socket.on("error", onError);
+    socket.on("close", onCompleted);
 
-        return () => {
-            socket.removeListener("message", onNext);
-            socket.removeListener("error", onError);
-            socket.removeListener("close", onCompleted);
-        };
+    return () => {
+        socket.removeListener("message", onNext);
+        socket.removeListener("error", onError);
+        socket.removeListener("close", onCompleted);
+    };
 
-    });
-};
+});
